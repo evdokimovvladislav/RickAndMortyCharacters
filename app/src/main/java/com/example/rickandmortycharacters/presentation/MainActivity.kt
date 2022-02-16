3d62994777a39cbadd07e918b428c542b87735ba
Mjko-1 <evdokimoff.vlad2011@yandex.ru>
2022-02-16 14:25:14 +0300

    Add data, domain and presentation directories.

--- app/src/main/java/com/example/rickandmortycharacters/presentation/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortycharacters/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rickandmortycharacters
+package com.example.rickandmortycharacters.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rickandmortycharacters.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
